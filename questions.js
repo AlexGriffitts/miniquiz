fb3a63820f3a959bd317f95a0011340ec5cf2078
AGriffitts <0alexgriffitts0@gmail.com>
2023-03-15 20:35:11 -0500

    whole thing

---
+++ questions.js
@@ -0,0 +1,40 @@
+var questions = [
+    {
+        question: "What is the capital of France?",
+        choices: ["Paris", "London", "Berlin", "Rome"],
+        correctAnswer: 0
+    },
+    {
+      question: "What four-legged animal is considered a marine mammal?",
+      choices: ["Dog", "Cat", "Dolphin", "Polar Bear"],
+      correctAnswer: 3
+    },
+    {
+      question: "In The Wizard of Oz, what was the lion in search of?",
+      choices: ["A heart", "A brain", "A home", "Courage"],
+      correctAnswer: 3
+    },
+    {
+      question: "What was the name of the military nobility of medieval and early-modern Japan?",
+      choices: ["Samurai", "Ninja", "Shogun", "Ronin"],
+      correctAnswer: 0
+    },
+    {
+      question: "What is the name of the largest ocean on Earth?",
+      choices: ["Pacific", "Atlantic", "Indian", "Arctic"],
+      correctAnswer: 0
+    },
+    {
+      question: "How many candles are lit throughout the eight nights of Hanukkah?",
+      choices: ["36", "8", "44", "24"],
+      correctAnswer: 2
+    },
+    {
+      question: "In mathematics, what term describes the bottom number in a fraction?",
+      choices: ["Denominator", "Numerator", "Divisor", "Dividend"],
+      correctAnswer: 0
+    }
+];
+
+
+
---
+++ script.js
@@ -0,0 +1,153 @@
+var startQuiz = document.querySelector("#startquiz");
+var startButton = document.querySelector("#startbutton");
+var time = document.querySelector("#seconds");
+var question = document.querySelector("#questions");
+var Quizchoices = document.querySelector("#answers");
+var choiceA = document.querySelector("#A");
+var choiceB = document.querySelector("#B");
+var choiceC = document.querySelector("#C");
+var choiceD = document.querySelector("#D");
+var score = document.querySelector("#score");
+var isCorrect = true;
+var saveScore = document.querySelector("#saveScore");
+var highscores = [];  
+var newForm = document.createElement("form");
+var input = document.createElement("input");
+var submit = document.createElement("input");
+
+//set the score
+var Userscore = 0;
+
+//start at first question
+var currentQuestion = 0;
+
+// set timer
+var time = 60;
+
+//start timer
+function startTime() {
+  time = 60;
+  Userscore = 0;
+  currentQuestion = 0;
+  var timer = setInterval(function() {
+    time--;
+    timer.textcontent = time;
+    }, 1000);
+  }
+
+//start quiz
+function startQuiz() {
+  startTime()
+  startButton.innerHTML = "";
+  score.innerHTML = "";
+  question.innerHTML = questions[currentQuestion].title;
+
+  //make buttons for choices
+  for (i = 0; i < 4; i++) {
+    var buttons = document.createElement("button");
+    buttons.innerHTML = questions[currentQuestion].choices[i];
+    Quizchoices.children[i].append(buttons);
+    buttons.className = "btn btn-primary btn-lg btn-block";
+  
+  }
+}
+
+//check if answer is correct
+choiceA.addEventListener("click", function() {
+  if (questions[currentQuestion].choices[0] === questions[currentQuestion].answer) {
+    isCorrect = true;
+    displayResult();
+  } else {
+    isCorrect = false;
+    displayResult();
+  }
+});
+
+choiceB.addEventListener("click", function() {
+  if (questions[currentQuestion].choices[1] === questions[currentQuestion].answer) {
+    isCorrect = true;
+    displayResult();
+  } else {
+    isCorrect = false;
+    displayResult();
+  }
+}); 
+
+choiceC.addEventListener("click", function() {  
+  if (questions[currentQuestion].choices[2] === questions[currentQuestion].answer) {
+    isCorrect = true;
+    displayResult();
+  } else {
+    isCorrect = false;
+    displayResult();
+  }
+});
+
+choiceD.addEventListener("click", function() {
+  if (questions[currentQuestion].choices[3] === questions[currentQuestion].answer) {
+    isCorrect = true;
+    displayResult();
+  } else {
+    isCorrect = false;
+    displayResult();
+  }
+}); 
+
+//display result function
+function displayResult() {
+  if (isCorrect) {
+    Userscore += 10;
+    score.innerHTML = "You Are Correct!";
+    increaseQuestion();
+  } else {
+    time -= 10;
+    score.innerHTML = "You Are Wrong!";
+    increaseQuestion();
+  }                       
+}
+
+//increase question function
+function increaseQuestion() {
+  currentQuestion++;
+  if (currentQuestion < questions.length || time > 0) {
+   question.innerHTML = "You have finished the quiz with " + Userscore + " points!";
+    choiceA.innerHTML = "";
+    choiceB.innerHTML = "";
+    choiceC.innerHTML = "";
+    choiceD.innerHTML = "";
+    score.innerHTML = "";
+    time.innerHTML = "";
+    highscore();
+  } else {
+    question.innerHTML = questions[currentQuestion].title;
+    
+    var choices = questions[currentQuestion].choices;
+    var buttons = [choiceA, choiceB, choiceC, choiceD];
+    for (i = 0; i < 4; i++) {
+      buttons[i].innerHTML = choices[i];
+
+
+    }
+  }
+
+  //highscore function
+  function highscore() {
+    score.appendChild(newForm);
+    input.setAttribute("type", "text");
+    input.setAttribute("name", "Enter Initials");
+    input.className = "setInitials";
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("value", "Submit");
+    submit.className = "submitInitials";
+    newForm.appendChild(input);
+    newForm.appendChild(submit);
+
+
+    newForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+     question.innerHTML= "High Scores";
+      var result = {initials: input.value, score: Userscore};
+    });
+}
+}
+startQuiz.addEventListener("click", startQuiz);
